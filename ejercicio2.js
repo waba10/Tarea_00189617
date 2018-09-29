e709e91b5e5c4727d3213e35c4f5ce0b8c1bfb7b
waba10 <00189617@uca.edu.sv>
2018-09-29 00:11:48 -0600

    first commit

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,38 +1,39 @@
 function conversion(num,unidad1,unidad2,medida){
-    if(unidad1==='m' & unidad2==='cm'){
+    var ob={cantidad:num, unidad1:unidad1, unidad2:unidad2,medida:medida};
+    if(ob.unidad1==='m' & ob.unidad2==='c'){
         return num*100;
     }
-    else if(unidad1==='c' & unidad2==='m'){
+    else if(ob.unidad1==='c' & ob.unidad2==='m'){
         return num/100;
     }
-    else if(unidad1==='k' & unidad2==='m'){
+    else if(ob.unidad1==='k' & ob.unidad2==='m'){
         return num*1000;
     }
-    else if(unidad1==='m' & unidad2==='k'){
+    else if(ob.unidad1==='m' & ob.unidad2==='k'){
         return num/1000;
     }
-    else if(unidad1==='p' & unidad2==='m'){
+    else if(ob.unidad1==='p' & ob.unidad2==='m'){
         return num/3.28084;
     }
-    else if(unidad1==='m' & unidad2==='p'){
+    else if(ob.unidad1==='m' & ob.unidad2==='p'){
         return num*3.28084;
     }
-    else if(unidad1==='C' & unidad2==='F'){
+    else if(ob.unidad1==='C' & ob.unidad2==='F'){
         return (num*(9/5))+32;
     }
-    else if(unidad1==='F' & unidad2==='C'){
+    else if(ob.unidad1==='F' & ob.unidad2==='C'){
         return ((num-32)*(9/5));
     }
-    else if(unidad1==='K' & unidad2==='F'){
+    else if(ob.unidad1==='K' & ob.unidad2==='F'){
         return ((num-273.15)*(9/5))+32;
     }
-    else if(unidad1==='F' & unidad2==='K'){
+    else if(ob.unidad1==='F' & ob.unidad2==='K'){
         return (((num-32)*(9/5))+32);
     }
-    else if(unidad1==='K' & unidad2==='C'){
+    else if(ob.unidad1==='K' & ob.unidad2==='C'){
         return num-273.15;
     }
-    else if(unidad1==='C' & unidad2==='K'){
+    else if(ob.unidad1==='C' & ob.unidad2==='K'){
         return num+273.15;
     }
     else{
